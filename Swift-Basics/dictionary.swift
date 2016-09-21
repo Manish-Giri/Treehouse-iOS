ba5243fc3c8a048bbc24e7b9dadbba28c37b78f1
MANISH-GIRI <manish.giri.me@gmail.com>
2016-09-20 23:10:17 -0400

    Challenge on dictionaries

---
+++ Swift-Basics/dictionary.swift
@@ -0,0 +1,10 @@
+// Enter your code below
+var iceCream = ["CC": "Chocolate Chip",
+"AP": "Apple Pie",
+"PB": "Peanut Butter"]
+
+iceCream.updateValue("Rocky Road", forKey: "RR")
+
+let applePie = iceCream["AP"]
+
+iceCream["CC"] = "Chocolate Chip Cookie Dough"
