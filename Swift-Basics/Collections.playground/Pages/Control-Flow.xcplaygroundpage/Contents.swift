a055faccf441154eeb69dd6cd91a532b29be8542
MANISH-GIRI <manish.giri.me@gmail.com>
2016-09-23 21:27:37 -0400

    Create new playground page for control flow

---
+++ Swift-Basics/Collections.playground/Pages/Control-Flow.xcplaygroundpage/Contents.swift
@@ -0,0 +1,21 @@
+//: [Previous](@previous)
+
+import Foundation
+
+var str = "Hello, playground"
+
+//: [Next](@next)
+
+//control flow
+var todo = ["Finish collections course",
+            "Buy groceries",
+            "Respond to emails",
+            "pick up book",
+            "order online",
+            "mow lawn"
+    
+]
+
+for item in todo {
+    print(item)
+}
