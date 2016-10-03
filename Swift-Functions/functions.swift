eb3e5b14fc8348655609f7e4d60c90610cf5fb4d
MANISH-GIRI <manish.giri.me@gmail.com>
2016-10-02 22:26:54 -0400

    Challenges on swift functions

---
+++ Swift-Functions/functions.swift
@@ -0,0 +1,16 @@
+// Enter your code below
+func temperatureInFahrenheit(temperature: Double) -> Double {
+
+  return (temperature * 9)/5 + 32
+
+}
+
+let fahrenheitTemp = temperatureInFahrenheit(24.0)
+
+
+// Enter your code below
+func getRemainder(value a: Int, divisor b: Int) -> Int {
+return a % b
+}
+
+let result = getRemainder(value: 10, divisor: 3)
