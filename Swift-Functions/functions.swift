b46e8385d2f7511a8b04c8befd423ca27da3db30
MANISH-GIRI <manish.giri.me@gmail.com>
2016-10-03 02:03:30 -0400

    Complete swift functions challenges and module

--- Swift-Functions/functions.swift
+++ Swift-Functions/functions.swift
@@ -14,3 +14,16 @@ return a % b
 }
 
 let result = getRemainder(value: 10, divisor: 3)
+
+
+// Enter your code below
+func getTowerCoordinates(location: String) -> (Double, Double) {
+
+switch location {
+case "Eiffel Tower": return (48.8582, 2.2945)
+case "Great Pyramid": return (29.9792, 31.1344)
+case "Sydney Opera House": return (33.8587, 151.2140)
+default: return(0, 0)
+
+}
+}
