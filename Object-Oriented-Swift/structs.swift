17be5890c9a56f524dc8783462f264efc0a69024
MANISH-GIRI <manish.giri.me@gmail.com>
2016-10-04 23:23:25 -0400

    Challenge on initializers

--- Object-Oriented-Swift/structs.swift
+++ Object-Oriented-Swift/structs.swift
@@ -26,3 +26,22 @@ struct Person {
 let aPerson = Person(firstName: "Tom", lastName: "Hanks")
 let fullName = aPerson.getFullName()
 
+//challenge on initializers
+struct RGBColor {
+    let red: Double
+    let green: Double
+    let blue: Double
+    let alpha: Double
+    
+    let description: String
+    
+    // Add your code below
+    init(red: Double, green: Double, blue: Double, alpha: Double) {
+        self.red = red
+        self.green = green
+        self.blue = blue
+        self.alpha = alpha
+        self.description = "red: \(red), green: \(green), blue: \(blue), alpha: \(alpha)"
+    }
+}
+
