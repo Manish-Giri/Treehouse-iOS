b919ed1cb2e1a2d05206d1fc6326a6c50bd7616f
MANISH-GIRI <manish.giri.me@gmail.com>
2016-10-04 23:23:07 -0400

    Playground on initializers

--- Object-Oriented-Swift/Obj-Oriented-Swift.playground/Contents.swift
+++ Object-Oriented-Swift/Obj-Oriented-Swift.playground/Contents.swift
@@ -6,6 +6,12 @@ struct Point {
     let x: Int
     let y: Int
     
+    init(x: Int, y: Int) {
+     self.x = x
+     self.y = y
+    
+    }
+    
     func surroundingPoints(withRange range: Int = 1) -> [Point] {
         var results: [Point] = []
         for xCoord in (x - range)...(x + range) {
@@ -24,3 +30,6 @@ p1.x
 p1.y
 p1.surroundingPoints()
 
+let p2 = Point(x: 4, y: 4)
+p2.x
+p2.y
