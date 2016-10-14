1f043e156d190fcc9a386ba1b117c901def519da
MANISH-GIRI <manish.giri.me@gmail.com>
2016-10-14 03:22:57 -0400

    Classes are pass by reference

--- Object-Oriented-Swift/ValueReference.playground/Contents.swift
+++ Object-Oriented-Swift/ValueReference.playground/Contents.swift
@@ -21,3 +21,26 @@ anotherUser.email
 someUser.email = "johndoe@xyz.com"
 someUser.email
 anotherUser.email
+
+
+class Person {
+    var fullName: String
+    var email: String
+    var age: Int
+    
+    init(fullName: String, email: String, age: Int) {
+        self.fullName = fullName
+        self.email = email
+        self.age = age
+    }
+    
+
+}
+
+var somePerson = Person(fullName: "Tim Cook", email: "tim.cook@apple.com", age: 54)
+somePerson.email
+
+var otherPerson = somePerson
+otherPerson.email
+somePerson.email = "tcook@apple.com"
+otherPerson.email
