34bcfdfc92a2615783feaa63aaa0fe11b94ea93f
MANISH-GIRI <manish.giri.me@gmail.com>
2016-10-16 17:13:26 -0400

    Create IBOutlet manually

---
+++ Simple-iPhone-App/viewcontroller.swift
@@ -0,0 +1,12 @@
+class ViewController: UIViewController {
+    @IBOutlet weak var textLabel: UILabel!
+
+    override func viewDidLoad() {
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        // Dispose of any resources that can be recreated
+    }
+
+}
