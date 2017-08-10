5c2e362b1aaeb0ec0dbf45cee5c7628555142fc5
MANISH-GIRI <manish.giri.me@gmail.com>
2017-08-10 02:16:38 -0400

    Add example for Value Type vs Reference Type

--- Object-Oriented-Swift/Obj-Oriented-Swift.playground/Contents.swift
+++ Object-Oriented-Swift/Obj-Oriented-Swift.playground/Contents.swift
@@ -1,145 +1,145 @@
 //: Playground - noun: a place where people can play
 
 import UIKit
-//
-//struct Point {
-//    let x: Int
-//    let y: Int
-//    
-//    init(x: Int, y: Int) {
-//     self.x = x
-//     self.y = y
-//    
-//    }
-//    
-//    func surroundingPoints(withRange range: Int = 1) -> [Point] {
-//        var results: [Point] = []
-//        for xCoord in (x - range)...(x + range) {
-//            for yCoord in (y - range)...(y + range) {
-//                let coordinatePoint = Point(x: xCoord, y: yCoord)
-//                results.append(coordinatePoint)
-//            }
-//            
-//        }
-//        return results
-//    }
-//}
-//
-//let p1 = Point(x: 2, y: 2)
-//p1.x
-//p1.y
-//p1.surroundingPoints()
-//
-//let p2 = Point(x: 4, y: 4)
-//p2.x
-//p2.y
-//
-//
-//
-////Classes
-//class Enemy {
-//    var life: Int = 2
-//    let position: Point
-//    init(x: Int, y: Int) {
-//        self.position = Point(x: x, y: y)
-//    }
-//    
-//    func decreaseHealth(factor: Int) {
-//        life -= factor
-//    }
-//}
-//
-//
-//class Tower {
-//    
-//    let position: Point
-//    var range: Int = 1
-//    //rate at which tower's health depletes
-//    var strength: Int = 1
-//    
-//    init(x: Int, y: Int) {
-//        self.position = Point(x: x, y: y)
-//    }
-//    
-//    func fireAtEnemy(enemy: Enemy) {
-//        
-//        if inRange(posiion: self.position, range: self.range, target: enemy.position) {
-//            //enemy is in range of the tower's position - fire 
-//            
-//            while(enemy.life > 0) {
-//                enemy.decreaseHealth(factor: strength)
-//                print("Enemy vanquished!!")
-//            }
-//        }
-//        else {
-//            print("Enemy outside range")
-//        }
-//        
-//    }
-//    
-//    func inRange(posiion: Point, range: Int, target: Point) -> Bool {
-//        
-//        //given a point (position) use the existing function to get an array of points
-//        //in the range given by the range paramter and check if the target point falls in that array
-//        
-//        let availablePoints = position.surroundingPoints(withRange: range)
-//        
-//        for point in availablePoints {
-//            if(point.x == target.x) && (point.y == target.y) {
-//                return true
-//            }
-//        }
-//        
-//        return false
-//
-//    }
-//}
-//
-//let tower = Tower(x: 0, y: 0)
-//let enemy = Enemy(x: 2, y: 1)
-//
-//tower.fireAtEnemy(enemy: enemy)
-//
-////inheritance
-//
-//class SuperEnemy: Enemy {
-//    
-//    let isSuper: Bool = true
-//    
-//    override init(x: Int, y: Int) {
-//        super.init(x: x, y: y)
-//        self.life = 50
-//    }
-//    
-//}
-//
-//
-//class LaserTower: Tower {
-//    
-//    override init(x: Int, y: Int) {
-//        super.init(x: x, y: y)
-//        self.strength = 100
-//        self.range = 100
-//    }
-//    
-//    override func fireAtEnemy(enemy: Enemy) {
-//        while enemy.life > 0 {
-//            enemy.decreaseHealth(factor: strength)
-//        }
-//        
-//     print("Enemy vanquished!!")
-//        
-//    }
-//}
-//
-//let laserTower = LaserTower(x: 0, y: 0)
-//
-//
-//let superEnemy = SuperEnemy(x: 20, y: 20)
-//superEnemy.life
-//
-//laserTower.fireAtEnemy(enemy: superEnemy)
-//superEnemy.life
+
+struct Point {
+    let x: Int
+    let y: Int
+    
+    init(x: Int, y: Int) {
+     self.x = x
+     self.y = y
+    
+    }
+    
+    func surroundingPoints(withRange range: Int = 1) -> [Point] {
+        var results: [Point] = []
+        for xCoord in (x - range)...(x + range) {
+            for yCoord in (y - range)...(y + range) {
+                let coordinatePoint = Point(x: xCoord, y: yCoord)
+                results.append(coordinatePoint)
+            }
+            
+        }
+        return results
+    }
+}
+
+let p1 = Point(x: 2, y: 2)
+p1.x
+p1.y
+p1.surroundingPoints()
+
+let p2 = Point(x: 4, y: 4)
+p2.x
+p2.y
+
+
+
+//Classes
+class Enemy {
+    var life: Int = 2
+    let position: Point
+    init(x: Int, y: Int) {
+        self.position = Point(x: x, y: y)
+    }
+    
+    func decreaseHealth(factor: Int) {
+        life -= factor
+    }
+}
+
+
+class Tower {
+    
+    let position: Point
+    var range: Int = 1
+    //rate at which tower's health depletes
+    var strength: Int = 1
+    
+    init(x: Int, y: Int) {
+        self.position = Point(x: x, y: y)
+    }
+    
+    func fireAtEnemy(enemy: Enemy) {
+        
+        if inRange(posiion: self.position, range: self.range, target: enemy.position) {
+            //enemy is in range of the tower's position - fire 
+            
+            while(enemy.life > 0) {
+                enemy.decreaseHealth(factor: strength)
+                print("Enemy vanquished!!")
+            }
+        }
+        else {
+            print("Enemy outside range")
+        }
+        
+    }
+    
+    func inRange(posiion: Point, range: Int, target: Point) -> Bool {
+        
+        //given a point (position) use the existing function to get an array of points
+        //in the range given by the range paramter and check if the target point falls in that array
+        
+        let availablePoints = position.surroundingPoints(withRange: range)
+        
+        for point in availablePoints {
+            if(point.x == target.x) && (point.y == target.y) {
+                return true
+            }
+        }
+        
+        return false
+
+    }
+}
+
+let tower = Tower(x: 0, y: 0)
+let enemy = Enemy(x: 2, y: 1)
+
+tower.fireAtEnemy(enemy: enemy)
+
+//inheritance
+
+class SuperEnemy: Enemy {
+    
+    let isSuper: Bool = true
+    
+    override init(x: Int, y: Int) {
+        super.init(x: x, y: y)
+        self.life = 50
+    }
+    
+}
+
+
+class LaserTower: Tower {
+    
+    override init(x: Int, y: Int) {
+        super.init(x: x, y: y)
+        self.strength = 100
+        self.range = 100
+    }
+    
+    override func fireAtEnemy(enemy: Enemy) {
+        while enemy.life > 0 {
+            enemy.decreaseHealth(factor: strength)
+        }
+        
+     print("Enemy vanquished!!")
+        
+    }
+}
+
+let laserTower = LaserTower(x: 0, y: 0)
+
+
+let superEnemy = SuperEnemy(x: 20, y: 20)
+superEnemy.life
+
+laserTower.fireAtEnemy(enemy: superEnemy)
+superEnemy.life
 
 // challenge
 class Vehicle {
@@ -187,3 +187,16 @@ let secondTestObject = SecondTest(x: 5, y: 4, z: 30)
 secondTestObject.x
 secondTestObject.y
 secondTestObject.z
+
+
+// small test for value type vs ref type
+var firstArr: [Int] = [4,5]
+var secondArr: [Int] = firstArr
+
+firstArr
+secondArr.append(40)
+secondArr
+firstArr
+firstArr.append(6)
+secondArr
+
